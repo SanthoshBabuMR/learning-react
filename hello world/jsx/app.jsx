914dae6e42188a21ad8159ed5ed916c75dfa7ca2
Babu Santhosh <babusa@BABUSA-MBP.local>
2016-07-22 21:53:29 +0530

    added hello world example

---
+++ hello world/jsx/app.jsx
@@ -0,0 +1,16 @@
+(function() {
+	'use strict';
+
+	var App = React.createClass({
+		render: function () {
+			return <div>Hello World</div>;
+		}
+	});
+
+	ReactDOM.render( 
+			<App />, 
+			document.getElementById('app') 
+	);
+
+})();	
+
